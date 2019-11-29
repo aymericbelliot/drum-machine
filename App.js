5936f46ef9545fe5be624dd60e96ef27f1dc0444
aymericbelliot <aymericbelliot@MacBook-de-Aymeric.local>
2019-11-28 23:45:01 -0400

    Initial layout

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App container-fluid">
-        <DrumBox/>
+        <DrumBox />
       </div>
     );
   }
@@ -17,9 +17,33 @@ class App extends React.Component {
 class DrumBox extends React.Component {
   render() {
     return (
-      <div className="card w-50 mt-5 shadow bg-primary text-white">
-        <div className="card-header d-flex justify-content-between align-items-center shadow">
-          Editor<i className="fas fa-expand-arrows-alt"></i>
+      <div id="drum-machine" className="card d-flex flex-sm-row p-3 shadow">
+        <div className="pad m-3">
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">Q</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">W</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">E</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">A</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">S</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">D</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">Z</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">X</button>
+          <button className="drum-pad btn btn-light btn-lg bg-secondary text-white">C</button>
+        </div>
+        <div className="controls d-flex flex-column justify-content-around align-items-center m-3">
+          <div class="custom-control custom-switch">
+            <input id="power" type="checkbox" className="custom-control-input"></input>
+            <label for="power" class="custom-control-label">Power</label>
+          </div>
+          <div class="form-group">
+            <input id="display" type="text" readonly className="form-control-plaintext bg-secondary text-white" value=""></input>
+          </div>
+          <div class="form-group">
+            <input type="range" class="form-control-range" id="volume"></input>
+          </div>
+          <div class="custom-control custom-switch">
+            <input id="bank" type="checkbox" className="custom-control-input"></input>
+            <label for="bank" class="custom-control-label">Bank</label>
+          </div>
         </div>
       </div>
     );
